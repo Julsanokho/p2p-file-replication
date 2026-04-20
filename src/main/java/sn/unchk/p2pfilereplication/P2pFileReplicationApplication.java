dbdbdc6796c67ca386afb472366a1780d19bc1ca
Souleymane Sanokho <sanokhosouleymane@gmail.com>
2026-04-20 13:38:31 +0000

    file replication set up

---
+++ src/main/java/sn/unchk/p2pfilereplication/P2pFileReplicationApplication.java
@@ -0,0 +1,13 @@
+package sn.unchk.p2pfilereplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2pFileReplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2pFileReplicationApplication.class, args);
+	}
+
+}
